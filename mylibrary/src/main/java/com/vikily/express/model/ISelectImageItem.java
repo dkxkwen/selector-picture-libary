cd70c023f2298b2481146b1ceeba6c375b48285a
xkw <dkxkwen@163.com>
2019-03-11 14:53:11 +0800

    first commit

---
+++ mylibrary/src/main/java/com/vikily/express/model/ISelectImageItem.java
@@ -0,0 +1,11 @@
+package com.vikily.express.model;
+
+/**
+ * Created by yixuanxuan on 16/10/12.
+ */
+
+public interface ISelectImageItem {
+    String getImgPath();
+    boolean isChecked();
+    void setChecked(boolean checked);
+}
