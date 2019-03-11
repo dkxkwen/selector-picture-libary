b49eecef4815c48ea3e7042b52823cebc5dd5b4b
xkw <dkxkwen@163.com>
2019-03-11 15:25:59 +0800

    test

--- app/src/main/java/com/vikily/express/MainActivity.java
+++ app/src/main/java/com/vikily/express/MainActivity.java
@@ -1,13 +1,29 @@
 package com.vikily.express;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.vikily.express.adapter.ImageAdapter;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final int REQUEST_CODE = 0x00000011;
+
+    private RecyclerView rvImage;
+    private ImageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvImage = findViewById(R.id.rv_image);
+        rvImage.setLayoutManager(new GridLayoutManager(this, 3));
+        mAdapter = new ImageAdapter(this);
+        rvImage.setAdapter(mAdapter);
+
     }
+
 }
